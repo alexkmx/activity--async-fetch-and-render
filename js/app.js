be908f2e5dc6e4273b266877b2e0d5ecf54b1bee
Alejandro <alexk_mx@hotmail.com>
2017-10-18 09:44:29 -0500

    firstCommit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+
+
 const request = superagent
+let resultsTable = document.querySelector('.users-list')
+
+document.querySelector('#app-container button').addEventListener('click', function() {
+
+request
+.get('https://randomuser.me/api/?results=50')
+.then(function(data){
+
+console.log(data.body.results);
+
+const results = data.body.results
+
+results.forEach(function(obj) {
+
+resultsTable.innerHTML += `
+
+<article>
+  <span>${obj.nat}</span>
+  <h6>${obj.email}</h6>
+</article>
+`
+})
+})
+})
